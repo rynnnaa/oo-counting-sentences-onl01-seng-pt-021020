58f5669401250bf391069da0fca6bc833eb43dfc
Ryna Truong <rynatruong@outlook.com>
2020-03-01 15:51:53 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,31 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.include?(".")
+      return TRUE
+    else 
+      FALSE
+    end
   end
 
   def question?
+    if self.include?("?")
+      return TRUE
+    else
+      FALSE
+    end
 
   end
 
   def exclamation?
-
+    if self.include?("!")
+      return TRUE
+    else
+      FALSE
+    end
   end
 
-  def count_sentences
-
-  end
+def count_sentences
+  self.split(/[.?!]+/).count
+end
 end
